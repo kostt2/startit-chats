040a803d6954a3c4a24515b71092f767a33d75ab
kostt2 <53828504+kostt2@users.noreply.github.com>
2020-01-10 17:05:07 +0200

    aaa

--- static/chats.js
+++ static/chats.js
@@ -1,26 +1,26 @@
 const ATJAUNOT = 1000;
 
-async function lasiChatu(){
+async function lasiChatu() {
     const atbilde = await fetch('/chats/lasi');
     const datuObjekts = await atbilde.json();
-    raadiChatuVienkarsi(datuObjekts);
+    raadiChataRindas(datuObjekts);
     await new Promise(resolve => setTimeout(resolve, ATJAUNOT));
     await lasiChatu();
 }
 
-function raadiChatuVienkarsi(dati){
+function raadiChatuVienkarsi(dati) {
     const jaunaRinda = "</br>";
     let chats = "";
     let chataDiv = document.getElementById("chats");
 
-    for(let rinda of dati['chats']){
+    for (let rinda of dati['chats']) {
         chats = chats + rinda + jaunaRinda;
     }
 
     chataDiv.innerHTML = chats;
 }
 
-async function suutiZinju(){
+async function suutiZinju() {
     let zinjasElements = document.getElementById('zinja');
     let zinja = zinjasElements.value;
 
@@ -31,20 +31,47 @@ async function suutiZinju(){
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({"chats": zinja})
+        body: JSON.stringify({ "chats": zinja })
     });
 
     const datuObjekts = await atbilde.json();
 
-    raadiChatuVienkarsi(datuObjekts);
-    
+    raadiChataRindas(datuObjekts);
+
 }
 
 
 let ievadesLauks = document.getElementById("zinja");
 
-ievadesLauks.addEventListener("keyup", function(event){
-    if(event.keyCode === 13){
+ievadesLauks.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
         suutiZinju();
     }
 })
+
+function raadiChataRindas(dati) {
+    const chatUL = document.getElementById("chats");
+    // novaacam ieprieksheejo saturu
+    while (chatUL.firstChild) {
+        chatUL.firstChild.remove();
+    }
+    for (let rinda of dati["chats"]) {
+        chatLI = izveidoJaunuRindu(rinda);
+        chatUL.appendChild(chatLI);
+    }
+    // noskrolleejam uz leju pie peedeejaa chata texta
+    var chatScrollBox = chatUL.parentNode;
+    chatScrollBox.scrollTop = chatScrollBox.scrollHeight;
+}
+
+
+function izveidoJaunuRindu(zinja) {
+    let newLI = document.createElement("li");
+    newLI.className = "left clearfix"
+    let newDiv = document.createElement("div");
+    newDiv.className = "chat-body clearfix"
+    let newContent = document.createTextNode(zinja);
+    newLI.appendChild(newDiv);
+    newDiv.appendChild(newContent);
+    return newLI;
+}
