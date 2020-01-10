cb3dd7ea6ee0288026f820971d0bdeb4b7a98f7a
kostt2 <53828504+kostt2@users.noreply.github.com>
2020-01-10 12:15:29 +0200

    ss

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ def index_page():
   return render_template('index.html')
 
 
+
+@app.route('/health')
+def health_check():
+  return "OK
+
+
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000, debug=True)
