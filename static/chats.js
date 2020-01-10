9252c4c39e9e5c5fa75267c0b72c234aef17de55
kostt2 <53828504+kostt2@users.noreply.github.com>
2020-01-10 17:08:04 +0200

    aaa

--- static/chats.js
+++ static/chats.js
@@ -1,6 +1,6 @@
 const ATJAUNOT = 1000;
 
-async function lasiChatu() {
+async function lasiChatu(){
     const atbilde = await fetch('/chats/lasi');
     const datuObjekts = await atbilde.json();
     raadiChataRindas(datuObjekts);
@@ -8,19 +8,19 @@ async function lasiChatu() {
     await lasiChatu();
 }
 
-function raadiChatuVienkarsi(dati) {
+function raadiChatuVienkarsi(dati){
     const jaunaRinda = "</br>";
     let chats = "";
     let chataDiv = document.getElementById("chats");
 
-    for (let rinda of dati['chats']) {
+    for(let rinda of dati['chats']){
         chats = chats + rinda + jaunaRinda;
     }
 
     chataDiv.innerHTML = chats;
 }
 
-async function suutiZinju() {
+async function suutiZinju(){
     let zinjasElements = document.getElementById('zinja');
     let zinja = zinjasElements.value;
 
@@ -31,23 +31,14 @@ async function suutiZinju() {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ "chats": zinja })
+        body: JSON.stringify({"chats": zinja})
     });
 
     const datuObjekts = await atbilde.json();
 
     raadiChataRindas(datuObjekts);
-
+    
 }
-
-
-let ievadesLauks = document.getElementById("zinja");
-
-ievadesLauks.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-        suutiZinju();
-    }
-})
 
 function raadiChataRindas(dati) {
     const chatUL = document.getElementById("chats");
@@ -56,22 +47,30 @@ function raadiChataRindas(dati) {
         chatUL.firstChild.remove();
     }
     for (let rinda of dati["chats"]) {
-        chatLI = izveidoJaunuRindu(rinda);
-        chatUL.appendChild(chatLI);
+      chatLI = izveidoJaunuRindu(rinda);
+      chatUL.appendChild(chatLI);
     }
     // noskrolleejam uz leju pie peedeejaa chata texta
     var chatScrollBox = chatUL.parentNode;
     chatScrollBox.scrollTop = chatScrollBox.scrollHeight;
-}
-
-
-function izveidoJaunuRindu(zinja) {
+  }
+  
+  
+  function izveidoJaunuRindu(zinja) { 
     let newLI = document.createElement("li");
     newLI.className = "left clearfix"
-    let newDiv = document.createElement("div");
+    let newDiv = document.createElement("div"); 
     newDiv.className = "chat-body clearfix"
-    let newContent = document.createTextNode(zinja);
-    newLI.appendChild(newDiv);
-    newDiv.appendChild(newContent);
+    let newContent = document.createTextNode(zinja); 
+    newLI.appendChild(newDiv); 
+    newDiv.appendChild(newContent); 
     return newLI;
-}
+  }
+
+let ievadesLauks = document.getElementById("zinja");
+
+ievadesLauks.addEventListener("keyup", function(event){
+    if(event.keyCode === 13){
+        suutiZinju();
+    }
+})
